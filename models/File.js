70f983e7b3114fb274c071940ee73f81fd248b05
Aniket Thapa <aniketthapa04@gmail.com>
2024-08-10 22:01:05 +0530

    minor-fixes

--- models/File.js
+++ models/File.js
@@ -4,6 +4,7 @@ const FileSchema = new mongoose.Schema({
   filename: { type: String, required: true },
   path: { type: String, required: true },
   shareableLink: { type: String, required: true, unique: true },
+  date_of_creation: { type: Date, default: Date.now },
 });
 
 module.exports = mongoose.model("File", FileSchema);
